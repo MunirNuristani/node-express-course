709055aeeecbe5bf610404292e9d972679cfc04f
MunirNuristani <nuristani.munir@gmail.com>
2022-12-19 22:51:15 -0800

    Week 6 completed.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,33 +1,53 @@
 const Task = require('../modals/tasks')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 
-const getAllTasks = async (req, res) => {
-    const task = await Task.find({})
-    res.status(200).json({ task })
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const task = await Task.find({})
+  res.status(200).json({ task })
+})
 
-} 
-
-const createTask = async(req, res)=>{
+const createTask = asyncWrapper(async (req, res) => {  
     const task = await Task.create(req.body)
-    res.status(201).json({task})
-}
+    res.status(201).json({ task })
+})
 
-const getTask = async(req, res)=>{
-    res.json({id:req.params.id})
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return next(createCustomError(`task with ${taskID} was not found`, 404))
+    }
+    res.status(200).json({ task })
+})
 
-const updateTask = (req, res)=>{
-    res.json({update: req.params.id})
-}
+const updateTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params
+    const { body } = req
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if (!task) {
+      return res.status(404).json({ msg: `task with ${taskID} was not found` })
+    }
+    res.status(200).json({ task })
+})
 
-const deleteTask = (req, res)=>{
-    res.json({Deleted: req.params.id })
-}
+const deleteTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ${taskID} ` })
+    }
+    res.status(200).json({ task })
+})
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
